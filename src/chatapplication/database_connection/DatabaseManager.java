419f3622d2f700b672a8c4ddb14422d9f45995da
GGJirka <jirkazboril4@seznam.cz>
2016-12-15 14:52:23 +0100

    Database manager - manager the database and query

---
+++ src/chatapplication/database_connection/DatabaseManager.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chatapplication.database_connection;
+
+import chatapplication.database_wrapper.SQLWrapper;
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.PreparedStatement;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author root
+ */
+public class DatabaseManager {
+    
+    public Connection connection;
+    public SQLWrapper wrapper;
+    
+    public DatabaseManager(String database, String username, String password) throws SQLException{
+        connection = (Connection) DriverManager.getConnection("jdbc:mysql://localhost/"+database+"?user="+username+"&password="
+                +password);
+        wrapper = new SQLWrapper();
+    }
+    
+    public PreparedStatement Select(Object[] columns ,String database, String where) throws SQLException{
+        return (PreparedStatement) connection.prepareStatement(wrapper.select(columns).from(database).where(where).getQuery());
+    }
+    
+    public PreparedStatement Delete(String table, String where) throws SQLException{
+        return (PreparedStatement) connection.prepareStatement(wrapper.delete(table).where(where).getQuery());       
+    }
+    
+    public PreparedStatement Insert(String table, Object[] values) throws SQLException{
+        return (PreparedStatement)connection.prepareStatement(wrapper.insert(table).values(values).getQuery());
+    }
+    
+    public PreparedStatement Update(String table, String word, String value, String where) throws SQLException{
+        return (PreparedStatement)connection.prepareStatement(wrapper.update(table).set(word, value).where(where).getQuery());
+    }
+}   
+
