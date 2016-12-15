c38f49a25af2128147da74745ce3e37a2c68b906
GGJirka <jirkazboril4@seznam.cz>
2016-12-15 14:00:08 +0100

    Added wrapper for SQL query

---
+++ src/chatapplication/database_wrapper/SQLWrapper.java
@@ -0,0 +1,121 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chatapplication.database_wrapper;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Jimmy
+ */
+public class SQLWrapper implements ISQLWrapper{
+    
+    private StringBuilder query;
+    
+    public SQLWrapper select(Object[] columns) {
+        query = new StringBuilder();
+        query.append("SELECT ");
+        if(columns.length != 0){
+            for(Object column : columns){
+                query.append(column);
+                query.append(", ");
+            }
+        }else{
+            query.append("* ");
+        }
+        query.deleteCharAt(query.lastIndexOf(","));
+        return this;
+    }
+
+    @Override
+    public SQLWrapper delete(String table) {
+        query = new StringBuilder();
+        query.append("DELETE FROM ");
+        query.append(table);
+        return this;
+    }
+
+    @Override
+    public SQLWrapper where(String charSequence) {
+        query.append(" WHERE ");
+        query.append(charSequence);
+        return this;
+    }
+
+    @Override
+    public SQLWrapper update(String table) {
+        query = new StringBuilder();
+        query.append("UPDATE ");
+        query.append(table);
+        return this;
+    }
+
+    @Override
+    public SQLWrapper set(Object[] words) {
+        if(words.length == 0){
+            try {
+                throw new Exception("words length is 0");
+            } catch (Exception ex) {
+                Logger.getLogger(SQLWrapper.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        query.append(" SET ");
+        for(Object word:words){
+            query.append(word);
+            query.append("=");
+            query.append("?");
+            query.append(", ");
+        }
+        query.deleteCharAt(query.lastIndexOf(","));
+        return this;
+    }
+    
+    @Override
+    public SQLWrapper set(String word){
+        query.append(" SET ");
+        query.append(word);
+        query.append("=");
+        query.append("?");
+        return this;
+    }
+    
+    @Override
+    public SQLWrapper set(String word, String newValue){
+        query.append(" SET ");
+        query.append(word);
+        query.append("=");
+        query.append(newValue);
+        return this;
+    }
+    
+    @Override
+    public SQLWrapper insert(String table) {
+        query = new StringBuilder();
+        query.append("INSERT INTO ");
+        query.append(table);
+        return this;
+    }
+  
+
+    @Override
+    public SQLWrapper values(Object[] values) {
+        query.append("VALUES");
+        query.append("(");
+        for(Object value:values){
+            query.append("?,");
+        }
+        query.deleteCharAt(query.lastIndexOf(","));
+        query.append(");");
+        return this;
+    }
+
+    @Override
+    public String getQuery() {
+        return query.toString();
+    }
+    
+}
